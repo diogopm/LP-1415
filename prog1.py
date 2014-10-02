75d93f4191ef4042b7d56913d7e310260105a9d3
Diogo Pina Manique <diogoprpm@gmail.com>
2014-10-02 21:34:39 +0100

    Programa 1
    Código adicional - variancia não funciona correctamente

--- prog1.py
+++ prog1.py
@@ -1,4 +1,6 @@
 
+import math
+import random
 
 print type(1), type(3.4), type((3+3j) + 2)
 
@@ -29,3 +31,40 @@ print type(a > 10 if 10 else 2)
 print type(a) == type("string") if "uma string" else "nao sei"
 
 print type(3j + 1.2)
+
+
+def randomList():
+	randomList = []
+	for i in range (0, 100):
+		randomList.append(random.randint(0, 200))
+
+	print randomList
+
+	return randomList
+
+
+
+def var(l1):
+	avg = sum(l1) / len(l1)
+	a = 0
+
+	print sum(l1)
+	print len(l1)
+
+	for x in l1:
+		a += ((x - avg) * (x - avg))
+
+	var = a / (len(l1) - 1)
+
+
+
+	return var
+
+
+print var(randomList())
+
+lista = ['a','b','c']
+#lista = [1,2,3,4,5]
+string_b = reduce(lambda x,y: x+y, lista)
+
+print string_b
