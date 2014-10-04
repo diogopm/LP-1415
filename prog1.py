78c7eebca3d0063570c8d35c5b92d384819ccd53
Diogo Pina Manique <diogoprpm@gmail.com>
2014-10-04 18:03:10 +0100

    Small changes

--- prog1.py
+++ prog1.py
@@ -1,8 +1,8 @@
-
+# coding=utf-8
 import math
 import random
 
-print type(1), type(3.4), type((3+3j) + 2)
+print type(1), type(3.4), type((3 + 3j) + 2)
 
 print type("treta"), type(True and False)
 
@@ -10,61 +10,67 @@ a = '1'
 b = "Uma"
 
 print type(a), type(b)
-print type([1,2,3,[2,3]]), type((1,2,3))
+print type([1, 2, 3, [2, 3]]), type((1, 2, 3))
 
+"""
 class Teste:
-	pass
+    def __init__(self):
+        pass
+
 
 print type(Teste)
 
 obj = Teste()
 print type(obj)
+"""
 
 def func():
-	return 3
+    return 3
 
-	print type(func)
+    print type(func)
 
-print type(range(0,10))
-print type(xrange(0,10))
+
+print type(range(0, 10))
+print type(xrange(0, 10))
 print type(a > 10 if 10 else 2)
 print type(a) == type("string") if "uma string" else "nao sei"
 
 print type(3j + 1.2)
 
 
-def randomList():
-	randomList = []
-	for i in range (0, 100):
-		randomList.append(random.randint(0, 200))
+def randomlist():
+    randlist = []
+    for i in range(0, 100):
+        randlist.append(random.randint(0, 200))
 
-	print randomList
+    print randlist
 
-	return randomList
-
-
-
-def var(l1):
-	avg = sum(l1) / len(l1)
-	a = 0
-
-	print sum(l1)
-	print len(l1)
-
-	for x in l1:
-		a += ((x - avg) * (x - avg))
-
-	var = a / (len(l1) - 1)
+    return randlist
 
 
+def var(lista):
+    """
 
-	return var
+    :rtype : object
+    """
+    avg = sum(lista) / len(lista)
+    print type(avg)
+    a = 0
+    print 'média: ' + str(avg)
+    #print 'somatório: ' + str(sum(lista))
+    print 'tamanho da lista: ' + str(len(lista))
+
+    for x in lista:
+        a += (x - avg)**2
+
+    var = a / (len(lista))
+    return var
 
 
-print var(randomList())
+print 'Variância: ' + str(var(randomlist()))
 
-lista = ['a','b','c']
-#lista = [1,2,3,4,5]
-string_b = reduce(lambda x,y: x+y, lista)
+lista = ['a', 'b', 'c']
+# lista = [1,2,3,4,5]
+string_b = reduce(lambda x, y: x + y, lista)
 
 print string_b
